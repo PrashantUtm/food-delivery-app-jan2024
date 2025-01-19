83c27d62cd3b796c93277b919b88c4e1dd7968e8
Prashant Purmah <prashant.purmah@sdworx.com>
2025-01-19 21:46:14 +0400

    Login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class LoginPage implements OnInit {
+  public username: string = '';
+  public password: string = '';
+  public invalidInput = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  public login(): void {
+    console.log(`${this.username} ${this.password}`);
+    if (this.authService.auth(this.username, this.password)) {
+      this.router.navigate(['/']);
+    } else {
+      this.invalidInput = true;
+    }
+  }
+
+  public onTextChanged(): void {
+    this.invalidInput = false;
+  }
 }
